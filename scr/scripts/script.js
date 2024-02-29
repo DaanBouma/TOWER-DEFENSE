a1b7c631dad4be31263cdf0850a6ca28a0bb7f7e
DaanBouma <130249739+DaanBouma@users.noreply.github.com>
2024-02-29 11:04:17 +0100

    Update
    update

--- scr/scripts/script.js
+++ scr/scripts/script.js
@@ -1,9 +1,10 @@
 ////////////////////////////////////////////////////////////////////
 
-let selectedMap = 0;
+let selectedMap = 1;
 let x = 0;
 let y = 0;
 
+const sand_tiles = [];
 const path = [];
 const enemies = [];
 let container_width = 0;
@@ -16,7 +17,7 @@ function setPosition(pos, x, y) {
 ////////////////////////////////////////////////////////////////////
 
 
-function mapAppear(){
+function mapAppear() {
     const game = document.querySelector(".game");
     setTimeout(function () {
         game.style.opacity = "1";
@@ -24,11 +25,6 @@ function mapAppear(){
 }
 
 mapAppear()
-
-
-
-
-
 
 ////////////////////////////////////////////////////////////////////
 
@@ -110,8 +106,8 @@ function sand_path(amount, typePath) {
             tilex: 0,
             tiley: 0,
             ID: 0,
-            centerX: 0,
             centerY: 0,
+            centerX: 0,
             Type: "sand"
         };
         tile.tilex = x;
@@ -120,11 +116,11 @@ function sand_path(amount, typePath) {
         tile.centerY = tile.tiley + 70;
         tile.ID = tileID;
         tileID += 1;
-        path.push(tile)
+        sand_tiles.push(tile)
         setPosition(tile.element, tile.tilex, tile.tiley)
         x += skip;
         container_width += skip;
-        
+
         return tile;
     }
     for (let i = 0; i < amount; i++) {
@@ -147,12 +143,12 @@ function createObject(objectType, min, max) {
         scr = "/scr/assets/objects/tree.png"
     } else if (objectType == "stone") {
         scr = "/scr/assets/objects/stone.png"
-    }  else if (objectType == "bush"){
+    } else if (objectType == "bush") {
         scr = "/scr/assets/objects/bush.png"
         const random2 = Math.random();
         const rotation2 = min + random2 * (max - min);
         rotation = rotation2;
-    } else if (objectType == "tree2"){
+    } else if (objectType == "tree2") {
         scr = "/scr/assets/objects/tree2.png"
     }
 
@@ -161,7 +157,7 @@ function createObject(objectType, min, max) {
         const element = document.createElement("img");
         element.src = scr;
         element.className = "object";
-        element.style.width = size + "px"; 
+        element.style.width = size + "px";
         element.draggable = false;
         element.style.marginLeft = "-" + (size / 2) + "px";
         element.style.marginTop = "-" + (size / 2) + "px";
@@ -193,7 +189,7 @@ function createObject(objectType, min, max) {
         const container = document.querySelector(".game");
         const element = document.createElement("div");
         element.style.borderRadius = "300px";
-        element.className = "border blockzones"; 
+        element.className = "border blockzones";
         element.draggable = false;
         element.style.backgroundColor = "red";
         element.style.width = borderSize + "px";
@@ -288,15 +284,15 @@ function generateObjects(mapping_objects) {
                 currentObjectY += 70;
             } else if (mappingID == -0) {
                 createObject("tree", 115, 130);
-            } else if (mappingID == -1){
+            } else if (mappingID == -1) {
                 currentObjectX += 100
-            } else if (mappingID == -2){
+            } else if (mappingID == -2) {
                 currentObjectX += 200
-            } else if (mappingID == -3){
+            } else if (mappingID == -3) {
                 createObject("stone", 50, 80);
-            } else if (mappingID == -4){
+            } else if (mappingID == -4) {
                 createObject("bush", 90, 125);
-            } else if (mappingID == -5){
+            } else if (mappingID == -5) {
                 createObject("tree2", 70, 80);
             }
         }
@@ -344,20 +340,21 @@ function createEnemy1(amount) {
         element.draggable = false;
         container.appendChild(element);
         let enemy = {
-            movingspeed: 10,
+            movingspeed: 100,
             element,
             enemyX: 0,
             enemyY: 0,
             ID: 0,
             Type: "Bee",
-            tilePassed: 0
+            tilePassed: 0,
         };
         EnemyID++
         enemy.ID = EnemyID;
         const tile = path[0]
         enemy.enemyX = tile.centerX
-        enemy.enemyY = tile.centerY - 140
-        setPosition(enemy.element, enemy.enemyX, enemy.enemyY - 17.5)
+        enemy.enemyY = tile.centerY
+        enemies.push(enemy)
+        setPosition(enemy.element, enemy.enemyX, enemy.enemyY - 35)
         return enemy;
     }
     for (let i = 0; i < amount; i++) {
@@ -367,11 +364,126 @@ function createEnemy1(amount) {
 
 ////////////////////////////////////////////////////////////////////
 
+let LT = Date.now();
+function update() {
+    const container = document.querySelector(".game")
+    const CT = Date.now();
+    const DT = (CT - LT) / 1000
+    updateEnemy(DT, container);
+    LT = CT
+    window.requestAnimationFrame(update);
+}
+
+let order = 0;
+let orderX = 0;
+let orderY = 0;
+
+function calOrder(){
+  if (order === 0){
+    path.push[sand_tiles[0]];
+    orderX = sand_tiles[0].centerX
+    orderY = sand_tiles[0].centerY
+    order++;
+  }  
+  console.log ("orderY: " + orderY + " orderX: " + orderX);
+}
+
+
+function calculateDirection(enemy) {
+    let direction = "";
+    const lastTile = enemy.tilePassed;
+    const nextTile = (lastTile + 1)
+
+    if (path[lastTile].centerY > path[nextTile].centerY) {
+        console.log("direction: up ");
+        direction = "up";
+    } else if (path[lastTile].centerY < path[nextTile].centerY) {
+        console.log("direction: down ");
+        direction = "down"; 
+    } else if (path[lastTile].centerX < path[nextTile].centerX) {
+        console.log("direction: right ");
+        direction = "right";
+    } else if (path[lastTile].centerX > path[nextTile].centerX) {
+        console.log("direction: left ");
+        direction = "left";
+    }
+    return direction;
+}
+
+
+function updateLasttile(enemy) {
+    let lastTile = enemy.tilePassed;
+    let nextTile = lastTile + 1;
+    const direction = calculateDirection(enemy);
+
+    
+    if (direction == "up") {
+        if (enemy.enemyY <= path[lastTile].centerY) {
+            enemy.tilePassed = nextTile
+        }
+    }
+    if (direction == "down") {
+        if (enemy.enemyY >= path[lastTile].centerY) {
+            enemy.tilePassed = nextTile
+        }
+    }
+    if (direction == "left") {
+        if (enemy.enemyX <= path[nextTile].centerX) {
+            enemy.tilePassed = nextTile
+        }
+    }
+    if (direction == "right") {
+        if (enemy.enemyX >= path[nextTile].centerX) {
+            enemy.tilePassed = nextTile
+        }
+    }
+}
+
+function updateEnemy(DT, container) {
+    for (let i = 0; i < enemies.length; i++) {
+        const enemy = enemies[i];
+        updateLasttile(enemy)
+        const direction = calculateDirection(enemy);
+        const movingspeed = enemy.movingspeed
+
+        if (direction == "up") {
+            const remove = (movingspeed * DT);
+            enemy.enemyY -= remove
+
+        }
+        if (direction == "down") {
+            const remove = (movingspeed * DT);
+            enemy.enemyY += (movingspeed * DT);
+        }
+        if (direction == "left") {
+            const remove = (movingspeed * DT);
+            enemy.enemyX -= (movingspeed * DT);
+        }
+        if (direction == "right") {
+            const remove = (movingspeed * DT);
+            enemy.enemyX += (movingspeed * DT);
+        }
+        if (direction == ""){
+            console.error("no direction asigned.")
+        }
+        setPosition(enemy.element, enemy.enemyX, enemy.enemyY);
+    }
+}
+
+
+
+////////////////////////////////////////////////////////////////////
+
 async function init() {
-    await generatePlayfield(selectedMap);
-    console.log(path);
-    console.log(path.length);
-    createEnemy1(1);
+    try {
+        await generatePlayfield(selectedMap);
+        console.log(path);
+        createEnemy1(1);
+        await window.requestAnimationFrame(update);
+    } catch (error) {
+        console.error('Initialization error:', error);
+    }
 }
 
 init();
+calOrder()
